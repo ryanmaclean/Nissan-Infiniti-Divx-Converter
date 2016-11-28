74cc854f2f8368372bbd952bca7d021409266e14
Ryan MacLean <ryan@ryanmaclean.com>
2016-11-27 23:11:50 -0800

    Update nissan_infiniti_divx.py

--- nissan_infiniti_divx.py
+++ nissan_infiniti_divx.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 inputfile = ''.join(args.inputarg)
 
 # These are the arguments used by ffmpeg to do the conversion
-ffmpegargs = "-f avi -r 29.97 -vcodec libxvid -vtag xvid -vf scale=720:-1 -aspect 16:9 -maxrate 8000k -b:v 6000k -qmin 3 -qmax 5 -bufsize 4096 -mbd 2 -bf 2 -trellis 1 -flags +aic -cmp 2 -subcmp 2 -g 300 -acodec ac3 -ar 48000 -aq 2"
+ffmpegargs = " -f avi -r 29.97 -vcodec libxvid -vtag xvid -vf scale=704:-1 -aspect 16:9 -maxrate 8000k -b:v 6000k -qmin 3 -qmax 5 -bufsize 4096 -mbd 2 -bf 2 -trellis 1 -flags +aic -cmp 2 -subcmp 2 -g 300 -acodec ac3 -ar 48000 -aq 2"
 
 # Remove the file extension using '.' as a separator, replace it with avi
 sep = '.'
